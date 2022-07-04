469d5caafe98fb0d006ca88bc27e3a84d5b0d58a
uutkukorkmaz <uutkukorkmaz>
2022-07-04 17:43:03 +0300

    Created Standing model
    Signed-off-by: uutkukorkmaz <uutkukorkmaz>

---
+++ database/migrations/2022_07_04_122417_create_standings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('standings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('week');
+            $table->foreignId('home_team_id')->constrained('teams')->cascadeOnDelete();
+            $table->foreignId('away_team_id')->constrained('teams')->cascadeOnDelete();
+            $table->integer('home_goals')->nullable();
+            $table->integer('away_goals')->nullable();
+            $table->integer('is_played')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('standings');
+    }
+
+};
