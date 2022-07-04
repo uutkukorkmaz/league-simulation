2b5cd851468711c8d15995be1ffb9115e8ccb65a
uutkukorkmaz <uutkukorkmaz>
2022-07-04 15:26:13 +0300

    Initialize league rules configuration
    Signed-off-by: uutkukorkmaz <uutkukorkmaz>

---
+++ config/league.php
@@ -0,0 +1,40 @@
+<?php
+
+
+return [
+    'name' => 'Premiere League',
+    'teams' => [
+        'Arsenal',
+        'Aston Villa',
+        'Bournemouth',
+        'Brentford',
+        'Brighton and Hove Albion',
+        'Chelsea',
+        'Crystal Palace',
+        'Everton',
+        'Fulham',
+        'Leeds United',
+        'Leicester City',
+        'Liverpool',
+        'Manchester City',
+        'Manchester United',
+        'Newcastle United',
+        'Nottingham Forest',
+        'Southampton',
+        'Tottenham Hotspur',
+        'West Ham United',
+        'Wolverhampton Wanderers',
+    ],
+
+    'fixture' => [
+        'rounds' => 2,
+    ],
+    'rules' => [
+        'points' => [
+            'win' => 3,
+            'draw' => 1,
+            'loss' => 0,
+        ],
+    ],
+
+];
