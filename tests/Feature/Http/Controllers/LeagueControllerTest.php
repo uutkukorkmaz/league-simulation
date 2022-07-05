2a8fb5cd93d130e11a660dab02a6ca8e5f0a7e63
uutkukorkmaz <uutkukorkmaz>
2022-07-05 22:02:17 +0300

    Refactored test for LeagueController
    Signed-off-by: uutkukorkmaz <uutkukorkmaz>

--- tests/Feature/Http/Controllers/LeagueControllerTest.php
+++ tests/Feature/Http/Controllers/LeagueControllerTest.php
@@ -1,22 +1,23 @@
 <?php
 
-namespace Tests\Feature;
+namespace Http\Controllers;
 
 use Tests\TestCase;
+use App\Models\Team;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class OverviewPageTest extends TestCase
+class LeagueControllerTest extends TestCase
 {
 
     use RefreshDatabase;
 
     /**
      * @test
-     * @covers \App\Http\Controllers\LeagueController::overview
+     * @covers \App\Http\Controllers\LeagueController::__invoke
      */
     public function overview_page_is_accessible()
     {
-        $response = $this->get(route('overview'));
+        $response = $this->get(route('standings'));
         $response->assertStatus(200);
     }
 
@@ -26,11 +27,9 @@ class OverviewPageTest extends TestCase
     public function overview_page_contains_all_teams()
     {
         $teams = config('league.teams');
-        $response = $this->get(route('overview'));
+        $response = $this->get(route('standings'));
         $response->assertStatus(200);
-        $response->assertViewHas('teams');
-        $response->assertViewHas('teams', $teams);
-        $response->assertSee($teams->pluck('name'), $teams);
+        $response->assertViewHas('teams', Team::all());
     }
 
     protected function setUp(): void
